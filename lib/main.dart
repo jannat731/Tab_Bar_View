cba534278dba2d513eb2bb76c906d3cf71668ff4
jannat731 <ummasoneyatuljannat33@gmail.com>
2022-01-27 09:46:09 +0600

    My First Commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car,size: 300,),
+              Icon(Icons.directions_transit,size: 300,),
+              Icon(Icons.directions_bike,size: 300,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
